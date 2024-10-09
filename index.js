9ee5270d274d03c791fe74598b9d72e415d40452
LAbreu00 <luisgabreu00@gmail.com>
2024-10-09 15:03:35 -0400

    finished Q5

--- index.js
+++ index.js
@@ -24,7 +24,48 @@ const greaterNum = (a, b) => {
   }
 };
 
-console.log(greaterNum(10, 7));
-console.log(greaterNum(1.14, 1.14));
-console.log(greaterNum("21", 21));
-console.log(greaterNum("21", "21"));
+//console.log(greaterNum(10, 7));
+//console.log(greaterNum(1.14, 1.14));
+//console.log(greaterNum("21", 21));
+//console.log(greaterNum("21", "21"));
+
+//Q3
+const sumOfFourAndSix = () => {
+  let s = 0;
+  for (i = 1; i <= 1000; i++) {
+    if (i % 4 === 0 && i % 6 === 0) {
+      s += i;
+    }
+  }
+  return s;
+};
+
+//console.log(sumOfFourAndSix());
+
+//Q4
+const oddAndEvenToN = (n) => {
+  let m = "";
+  for (i = 0; i <= n; i++) {
+    if (i % 2 === 0) {
+      m = `${i} is even`;
+    } else {
+      m = `${i} is odd`;
+    }
+    console.log(m);
+  }
+};
+
+//oddAndEvenToN(5);
+
+//Q5
+const sumOfNotDivisibleByTen = () => {
+  let s = 0;
+  for (i = 1; i <= 1000; i++) {
+    if (i % 10 !== 0) {
+      s += i;
+    }
+  }
+  return s;
+};
+
+//console.log(sumOfNotDivisibleByTen());
